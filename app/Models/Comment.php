13e254cfbd79e8ccf94e1a066eefbc5c0356adf3
Joaquin Pereira <pereira.joaquin@gmail.com>
2023-02-06 12:34:41 -0400

    agregando trait para likes con métodos comunes.

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use App\Traits\HasLikes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    use HasFactory;
+    use HasFactory, HasLikes;
 
     protected $guarded = [];
 
@@ -16,32 +17,4 @@ class Comment extends Model
         return $this->belongsTo(User::class);
     }
 
-    public function likes()
-    {
-        return $this->morphMany(Like::class, 'likeable');
-    }
-
-    public function like()
-    {
-        $this->likes()->firstOrCreate([
-            'user_id' => auth()->id()
-        ]);
-    }
-
-    public function unlike()
-    {
-        $this->likes()->where([
-            'user_id' => auth()->id()
-        ])->delete();
-    }
-
-    public function isLiked()
-    {
-        return $this->likes()->where('user_id',auth()->id())->exists();
-    }
-
-    public function likesCount()
-    {
-        return $this->likes()->count();
-    }
 }
--- app/Models/Status.php
+++ app/Models/Status.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use App\Traits\HasLikes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
-    use HasFactory;
+    use HasFactory, HasLikes;
 
     protected $guarded = [];
 
@@ -16,38 +17,9 @@ class Status extends Model
         return $this->belongsTo(User::class);
     }
 
-    public function likes()
-    {
-        return $this->morphMany(Like::class, 'likeable');
-    }
-
     public function comments()
     {
         return $this->hasMany(Comment::class);
-    }
-
-    public function like()
-    {
-        $this->likes()->firstOrCreate([
-            'user_id' => auth()->id()
-        ]);
-    }
-
-    public function unlike()
-    {
-        $this->likes()->where([
-            'user_id' => auth()->id()
-        ])->delete();
-    }
-
-    public function isLiked()
-    {
-        return $this->likes()->where('user_id',auth()->id())->exists();
-    }
-
-    public function likesCount()
-    {
-        return $this->likes()->count();
     }
 
 }
