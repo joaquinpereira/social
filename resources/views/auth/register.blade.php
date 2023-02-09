0e85c068025b6e0ecf80ea6cc9e6033b04249c50
Joaquin Pereira <pereira.joaquin@gmail.com>
2023-02-09 06:08:17 -0400

    probando el registro con dusk

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,10 +12,11 @@
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-end">UserName:</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name"
+                                    value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="Nombre de usuario...">
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -26,10 +27,43 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                            <label for="first_name" class="col-md-4 col-form-label text-md-end">Nombre:</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="first_name" type="text" class="form-control @error('first_name') is-invalid @enderror"
+                                    name="first_name" value="{{ old('first_name') }}" required autocomplete="first_name" autofocus
+                                    placeholder="Tu nombre...">
+
+                                @error('first_name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="last_name" class="col-md-4 col-form-label text-md-end">Apellido:</label>
+
+                            <div class="col-md-6">
+                                <input id="last_name" type="text" class="form-control @error('last_name') is-invalid @enderror"
+                                    name="last_name" value="{{ old('last_name') }}" required autocomplete="last_name" autofocus
+                                    placeholder="Tu Apellido...">
+
+                                @error('last_name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">Email:</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                                    name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Tu email...">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -40,10 +74,11 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-end">Contraseña:</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
+                                    name="password" required autocomplete="new-password" placeholder="Tu contraseña..">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -57,13 +92,14 @@
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation"
+                                    required autocomplete="new-password"  placeholder="Repite tu contraseña..">
                             </div>
                         </div>
 
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary" id="register-btn">
                                     {{ __('Register') }}
                                 </button>
                             </div>
--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -18,6 +18,10 @@ class RegistrationTest extends TestCase
     /** @test */
     public function users_can_register()
     {
+        $this->withoutExceptionHandling();
+
+        $this->get(route('register'))->assertSuccessful();
+
         $response = $this->post(route('register'), $this->userValidData());
 
         $response->assertRedirect('/');
